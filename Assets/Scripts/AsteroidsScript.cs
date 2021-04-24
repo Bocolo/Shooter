4900c19a037a0caeef9f8bd9ca1bff7f380384c6
bocolo <bronaghcolt@hotmail.com>
2021-04-24 21:55:07 +0100

    Player movement added. Falling asteroids added-> to be updated with pooled arrays, random spawning, speeds,rotation and continuos spawning

---
+++ Assets/Scripts/AsteroidsScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidsScript : MonoBehaviour
+{
+
+    public GameObject[] asteroids;
+    private void Start()
+    {
+        SpawnAsteroid();
+    }
+    void SpawnAsteroid()
+    {
+        
+        for (int i = 0; i < asteroids.Length; i++)
+        {
+            Vector3 position = new Vector3(Random.Range(-2.2f, 2.2f), 6, 0);
+            Instantiate(asteroids[i], position, Quaternion.identity);
+        }
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    Rigidbody2D rb;
+    public float speed;
+  
+    [SerializeField] [Range(0, 1)] float LerpConstant;
+    private void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
-
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+
+        Movement();
+    }
+    private void Movement()
+    {
+
+        if (transform.position.x <= -2.2f)
+        {
+            transform.position = new Vector2(-2.2f, transform.position.y);
+        }
+        else if (transform.position.x >= 2.2f)
+        {
+            transform.position = new Vector2(2.2f, transform.position.y);
+        }
+
+        float h = Input.GetAxisRaw("Horizontal");
+        Vector2 movement = new Vector2(h, rb.velocity.y);
+        rb.velocity = Vector2.Lerp(rb.velocity, movement, LerpConstant) * speed;
+
+
     }
 }
